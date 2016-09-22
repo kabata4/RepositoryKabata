4b25ec205338d50c2671d4868e31d7c554f6313f
Razumnikov Alexander <kabata4@users.noreply.github.com>
2016-09-22 10:00:34 +0400

    Update zadacha1906164.java

--- SistemnoPO/zadacha1906164.java
+++ SistemnoPO/zadacha1906164.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-	public class zadacha1906164 {
+	public class zadacha1909164 {
 	public static void main(String[] args) 
 	{
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Введите число: "); 
+		System.out.println("Г‚ГўГҐГ¤ГЁГІГҐ Г·ГЁГ±Г«Г®: "); 
 		double a = scanner.nextDouble();
-		System.out.println("Введите число: "); 
+		System.out.println("Г‚ГўГҐГ¤ГЁГІГҐ Г·ГЁГ±Г«Г®: "); 
 		double b = scanner.nextDouble();
 		double x = (2+a*b)/(Math.pow(a, 2)-Math.pow(b, 2));
 		double y = a/3+7*Math.pow(b, 3);
